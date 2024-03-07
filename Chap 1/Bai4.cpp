5d13952fdbc322bda3e255cadcbced969ddd6933
Harry1720 <22h1120002@ut.edu.vn>
2024-03-07 11:42:01 +0700

    up bai 4

---
+++ Chap 1/Bai4.cpp
@@ -0,0 +1,10 @@
+//Viết chương trình nhập và xuất 1 kí tự.
+#include <iostream>
+using namespace std;
+int main (){
+	cout << "Nhap mot ky tu: ";
+	char a; cin >> a;
+	cout << "Ky tu vua nhap: " << a;
+
+	return 0;
+} 
